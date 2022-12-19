bc0d0bf370196dba3a7c009acb9edfe4d77f23fd
yuriiklimenko <109259174+yuriiklimenko@users.noreply.github.com>
2022-12-19 21:45:37 +0200

    fix render

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,28 +1,23 @@
-import LoadMoreBtn from './load-more-btn'
+import LoadMoreBtn from './load-more-btn';
 
-const container = document.querySelector('body')
+const container = document.querySelector('body');
 
 // window.addEventListener('load', function() {
-    // displayLoader();
+// displayLoader();
 // });
 
 export function displayLoader() {
-    // container.classList.add('.loader-background');
-    // container.insertAdjacentHTML('afterbegin', '<div class="loader"></div>');
-    document.querySelector('.loader').classList.remove('disable');
+  // container.classList.add('.loader-background');
+  // container.insertAdjacentHTML('afterbegin', '<div class="loader"></div>');
+  document.querySelector('.loader').classList.remove('disable');
 }
 
 export function disableLoader() {
-    setTimeout(() => {
-        document.querySelector('.loader').classList.add('disable');    
-    }, 400);
-    // container.classList.add('.loader-background');
-    
+  setTimeout(() => {
+    document.querySelector('.loader').classList.add('disable');
+  }, 400);
+  // container.classList.add('.loader-background');
 }
-
-
-
-
 
 // const loadMoreBtn = new LoadMoreBtn({
 //     selector: '[data-action="load-more"]',
